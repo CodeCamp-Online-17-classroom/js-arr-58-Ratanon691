f857b4edb8c4de8154d5bf326a3deff50d6d5884
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-07-14 10:12:59 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// write code here
+const persons = [
+  { name: 'John', sex: 'M' },
+  { name: 'Jody', sex: 'M' },
+  { name: 'Susan', sex: 'F' },
+  { name: 'Kate', sex: 'F' },
+  { name: 'Sid', sex: 'M' }
+];
+const groupedBySex = persons.reduce((acc, person) => {
+  if (!acc[person.sex]) {
+    acc[person.sex] = []
+  }
+  acc[person.sex].push(person.name)
+  return acc
+}, {})
+
+console.log(groupedBySex)
+// Output: { M: ['John', 'Jody', 'Sid'], F: ['Susan', 'Kate'] }
